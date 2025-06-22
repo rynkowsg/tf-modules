fd3786bd6cbb7c52a8b5af54fb19641721640572
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2025-06-22 11:13:53 +0200

    feat: Add 'iam_roles'

---
+++ module/iam-roles/main.tf
@@ -0,0 +1,60 @@
+# --------------------
+#  IAM roles
+# --------------------
+
+resource "aws_iam_role" "role" {
+  for_each = { for idx, r in var.roles : r.name => r }
+
+  name = each.value.name
+  assume_role_policy = each.value.assume_role_policy
+}
+
+# --------------------------
+#  Inline IAM role policies
+# --------------------------
+
+locals {
+  inline_policies = flatten([
+    for r in var.roles : [
+      for ip in r.inline_policy: {
+        role_name: r.name
+        inline_policy_name: ip.name
+        inline_policy_content: ip.content
+      }
+    ]
+  ])
+}
+
+resource "aws_iam_role_policy" "inline_policy" {
+  for_each = { for idx, ip in local.inline_policies: "${ip["role_name"]}--${ip["inline_policy_name"]}" => ip }
+
+  depends_on = [aws_iam_role.role]
+
+  role = each.value["role_name"]
+  name = each.value["inline_policy_name"]
+  policy = each.value["inline_policy_content"]
+}
+
+# ------------------------------
+#  IAM roles policy attachments
+# ------------------------------
+
+locals {
+  policy_attachments = flatten([
+    for r in var.roles : [
+      for rarn in r.attached_policy_arns: {
+        role_name: r.name
+        arn: rarn
+      }
+    ]
+  ])
+}
+
+resource "aws_iam_role_policy_attachment" "policy_attachment" {
+  for_each   = { for idx, pa in local.policy_attachments: "${pa["role_name"]}--${basename(pa["arn"])}" => pa }
+
+  depends_on = [aws_iam_role.role]
+
+  role       = each.value["role_name"]
+  policy_arn = each.value["arn"]
+}
