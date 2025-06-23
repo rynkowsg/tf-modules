9a131a82287cb6ed9ae24074d11925c72504c65e
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2025-06-23 18:08:26 +0200

    feat!: Add key info in bucket-s3-kms output

--- module/backend-s3-kms/outputs.tf
+++ module/backend-s3-kms/outputs.tf
@@ -4,3 +4,12 @@ output "bucket" {
     "name" : aws_s3_bucket.terraform_state.bucket,
   }
 }
+
+output "kms_key" {
+  value = {
+    "arn" : aws_kms_key.terraform_state.arn,
+    "id" : aws_kms_key.terraform_state.id,
+    "alias": aws_kms_alias.terraform_state.name,
+    "alias_arn": aws_kms_alias.terraform_state.arn,
+  }
+}
